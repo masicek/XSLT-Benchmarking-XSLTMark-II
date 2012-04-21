8959126880e7eedcebc283fca151e6e538c232de
Viktor Masicek <viktor@masicek.net>
2012-04-21 20:12:44 +0200

    run.bat: No php.ini file will be used

--- run.bat
+++ run.bat
@@ -1,1 +1,1 @@
-.\Libs\Php\5.3.6\php.exe -d extension=.\Libs\Php\5.3.6\php_mbstring.dll .\XSLTBenchmarking\index.php %*
+.\Libs\Php\5.3.6\php.exe -n -d extension=.\Libs\Php\5.3.6\php_mbstring.dll .\XSLTBenchmarking\index.php %*
